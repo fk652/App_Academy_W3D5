4b1d68b4a7dbddda3146f138097d47a2d1638cd7
Fahim Khan <fk652@nyu.edu>
2023-01-27 13:57:47 -0500

    add initialize, value, parent

--- skeleton/skeleton/lib/00_tree_node.rb
+++ skeleton/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,23 @@
 class PolyTreeNode
+  node_1 = PolyTreeNode.new(root)
+  node_2 = PolyTreeNode.new(child_1)
+  node_3 = PolyTreeNode.new(child_2)
 
+  def initialize
+    new_node = PolyTreeNode.new(new_node)
+    self.value = new_node
+    new_node.parent = nil
+    new_node.children = []
+  end
+
+  def value
+    @value
+  end 
+
+  def parent=(parent_node)
+    if parent_node != nil
+      self.parent = parent_node
+      parent_node.children << pare
+    end
+  end
 end
