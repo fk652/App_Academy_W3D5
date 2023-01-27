2acfd5f2becb110751a6422a28b689f42d272493
Fahim Khan <fk652@nyu.edu>
2023-01-27 14:49:53 -0500

    add dfs, remove_child, add_child

--- skeleton/skeleton/lib/00_tree_node.rb
+++ skeleton/skeleton/lib/00_tree_node.rb
@@ -19,9 +19,9 @@ class PolyTreeNode
 
   def parent=(new_parent)
       if new_parent != nil
-        if @parent = nil 
+        if @parent != nil 
           @parent.children.delete(self)     
-        # end 
+        end 
         @parent = new_parent
         new_parent.children << self 
       elsif 
@@ -29,4 +29,24 @@ class PolyTreeNode
         @parent = nil
       end     
   end
+
+  def add_child(new_child)
+    new_child.parent = self
+  end
+
+  def remove_child(new_child)
+    new_child.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if @value == target_value
+
+    children.each do |child|
+      result = child.dfs(target_value)
+      return result if result != nil
+    end
+    nil
+  end
+
+  
 end
