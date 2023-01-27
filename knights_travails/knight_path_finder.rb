e3d7a992cc48d9b52075cec8567512bcd087d801
Fahim Khan <fk652@nyu.edu>
2023-01-27 15:19:55 -0500

    knights travails

---
+++ knights_travails/knight_path_finder.rb
@@ -0,0 +1,15 @@
+require_relative "poly_tree_node"
+
+class KnightPathFinder
+  def initialize(position)
+    @root_node = position
+    self.build_move_tree
+  end
+
+  def build_move_tree
+    # root = PolyTreeNode.new(root_node)
+
+  end
+
+  
+end
---
+++ knights_travails/poly_tree_node.rb
@@ -0,0 +1,77 @@
+       #   a
+      # b   c
+    # d e    f
+
+#     e = Node.new('e')
+# f = Node.new('f') 
+# g = Node.new('g') 
+# b = Node.new('b', [d, e])
+# c = Node.new('c', [f, g])
+# a = Node.new('a', [b, c])
+
+
+class PolyTreeNode
+
+
+
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value 
+    @parent = nil   #parent is an attribute that refers to a node
+    @children = []  #refers to a bunch 
+  end
+
+  def value
+    @value
+  end 
+
+
+  def parent=(new_parent)
+      if new_parent != nil
+        if @parent != nil 
+          @parent.children.delete(self)     
+        end 
+        @parent = new_parent
+        new_parent.children << self 
+      elsif 
+        @parent.children.delete(self)
+        @parent = nil
+      end     
+  end
+
+  def add_child(new_child)
+    new_child.parent = self
+  end
+
+  def remove_child(new_child)
+    new_child.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if @value == target_value
+
+    children.each do |child|
+      result = child.dfs(target_value)
+      return result if result != nil
+    end
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [] 
+    queue.push(self) 
+    until queue.empty?
+
+      current_node = queue.shift
+      return current_node if current_node.value == target_value
+
+      current_node.children.each do |child|
+        queue.push(child)
+      end
+    end
+    nil
+  end
+
+
+end
