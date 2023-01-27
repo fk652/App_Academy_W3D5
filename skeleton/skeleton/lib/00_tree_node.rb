76d59f2714631d68934faaa2b1b9aa383542a2ec
Jordan Buranskas <jordanburanskas@gmail.com>
2023-01-27 14:36:03 -0500

    whoops

--- skeleton/skeleton/lib/00_tree_node.rb
+++ skeleton/skeleton/lib/00_tree_node.rb
@@ -1,23 +1,32 @@
 class PolyTreeNode
-  node_1 = PolyTreeNode.new(root)
-  node_2 = PolyTreeNode.new(child_1)
-  node_3 = PolyTreeNode.new(child_2)
 
-  def initialize
-    new_node = PolyTreeNode.new(new_node)
-    self.value = new_node
-    new_node.parent = nil
-    new_node.children = []
+       #   a
+      # b   c
+    # d e    f
+
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value 
+    @parent = nil   #parent is an attribute that refers to a node
+    @children = []  #refers to a bunch 
   end
 
   def value
     @value
   end 
 
-  def parent=(parent_node)
-    if parent_node != nil
-      self.parent = parent_node
-      parent_node.children << pare
-    end
+
+  def parent=(new_parent)
+      if new_parent != nil
+        if @parent = nil 
+          @parent.children.delete(self)     
+        # end 
+        @parent = new_parent
+        new_parent.children << self 
+      elsif 
+        @parent.children.delete(self)
+        @parent = nil
+      end     
   end
 end
